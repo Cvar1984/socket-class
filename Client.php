4c4cfe2574cb4fcd5480fb8c1136eb2da986fb9f
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2019-12-24 20:22:17 +0700

    rename

--- Client.php
+++ Client.php
@@ -9,7 +9,7 @@ class Client
         self::$input = $message;
     }
 
-    public static function listenConnection(string $host = '127.0.0.1', int $port)
+    public static function connect(string $host = '127.0.0.1', int $port)
     {
         $sock = socket_create(AF_INET, SOCK_STREAM, 0);
         $result = socket_connect($sock, $host, $port);
@@ -21,7 +21,7 @@ class Client
 try {
     $message = readline('Message : ');
     Client::setMessage($message);
-    echo Client::listenConnection('127.0.0.1', 40141);
+    echo Client::connect('127.0.0.1', 40141);
 } catch (Exception $e) {
     fprintf(STDERR, '%s%s', $e->getMessage(), PHP_EOL);
     exit(1);
